7be2729f0e862741a87aef90b9b1418adf28d55d
Spandana44 <reddyspandana4444@gmail.com>
2020-12-21 15:09:54 +0530

    Changes made again

--- Calculator/src/main/java/com/lti/Calci.java
+++ Calculator/src/main/java/com/lti/Calci.java
@@ -10,8 +10,13 @@ public class Calci {
 		return a - b;
 	}
 	
+	public static int prod(int a , int b) {
+		return a * b;
+	}
+	
 	public static void main(String args[]) {
 		System.out.println("Sum = " + add(10 , 20));
 		System.out.println("diff = " + subs(20,10));
+		System.out.println("product = " + prod(20,10));
 	}
 }
